766f9ce7ebdac4d41cf298c22de8821774dd4b71
ilay Serr <ilay92@gmail.com>
2018-01-06 14:19:16 +0200

    adding changes

--- src/DownloadableMetadata.java
+++ src/DownloadableMetadata.java
@@ -1,5 +1,9 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
+
+import javax.xml.transform.Templates;
+
 
 /**
  * Describes a file's metadata: URL, file name, size, and which parts already downloaded to disk.
@@ -14,21 +18,50 @@ class DownloadableMetadata {
     private final String metadataFilename;
     private String filename;
     private String url;
-    private boolean completed;
-    private Range range;
-    private File metadataFile;
-    private long size;
     
-    DownloadableMetadata(String url) {
+    //private boolean completed;
+    private long size;
+    private boolean [] chunksArr;
+    private File metadataFile;
+    private int chunk_size;
+    
+    DownloadableMetadata(String url) throws MalformedURLException {
         this.url = url;
         this.filename = getName(url);
         this.metadataFilename = getMetadataName(filename);
-        this.completed = false;
-        this.size = 0;
-        this.range = new Range((long)0, (long)0);      
+        this.metadataFile = new File(this.metadataFilename);
         
+        // this.completed = false;
+        this.size = getFileSize(new URL(url));     
+        this.chunk_size = 4096;
         
+        // in case this a small file with less then 4096 bytes.
+        if (size < chunk_size) {
+        	chunk_size = (int)size;
+        }
+        this.chunksArr = new boolean [(int)(Math.ceil(size / chunk_size))];
     }
+    
+    public void write_file () {
+    	File temp = new File("temp.metadata");
+    	
+	}
+    
+//    public void fetch_from_file (String filename) throws FileNotFoundException {
+//    	// Creating File instance to reference text file in Java
+//        File text = new File(filename);
+//        
+//        // Creating Scanner instnace to read File in Java
+//        Scanner scnr = new Scanner(text);
+//      
+//        // Reading each line of file using Scanner class
+//        int lineNumber = 1;
+//        while(scnr.hasNextLine()){
+//            String line = scnr.nextLine();
+//            System.out.println("line " + lineNumber + " :" + line);
+//            lineNumber++;
+//        } 
+//    }
 
     private static String getMetadataName(String filename) {
         return filename + ".metadata";
@@ -37,26 +70,57 @@ class DownloadableMetadata {
     private static String getName(String path) {
         return path.substring(path.lastIndexOf('/') + 1, path.length());
     }
-
+    
+    // update the location in the array to be true
     void addRange(Range range) {
-    	if (this.range.getEnd() == range.getStart())
-    		this.range = new Range(this.range.getStart(), range.getEnd());
+    	chunksArr[(int)(range.getStart() / chunk_size) - 1] = true;
+	//    	if (this.range.getEnd() == range.getStart())
+	//    		this.range = new Range(this.range.getStart(), range.getEnd());
+    	
     }
 
+    private static long getFileSize(URL url) {
+        URLConnection conn = null;
+        try {
+            conn = url.openConnection();
+            if(conn instanceof HttpURLConnection) {
+                ((HttpURLConnection)conn).setRequestMethod("HEAD");
+            }
+            conn.getInputStream();
+            return conn.getContentLengthLong();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(conn instanceof HttpURLConnection) {
+                ((HttpURLConnection)conn).disconnect();
+            }
+        }
+    }
+    
     String getFilename() {
         return filename;
     }
 
     boolean isCompleted() {
-        return completed;
+        for (int i = 0; i < chunksArr.length; i++) {
+			if (!chunksArr[i]) return false;
+		}
+        return true;
+    	//return completed;
     }
 
     void delete() {
         //TODO
     }
 
+    // return the first available chunk size
     Range getMissingRange() {
-        //TODO
+    	for (int i = 0; i < chunksArr.length; i++) {
+ 			if (!chunksArr[i]) return (new Range(i * (long)chunk_size, (i + 1) * (long)chunk_size));
+ 		}
+    	
+    	// if completed
+		return null;
     }
 
     String getUrl() {
